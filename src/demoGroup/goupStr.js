17c5e9ea02b82b03de2f2dc1234eaee23970bea4
wxamlzm <zd5024@163.com>
2024-01-05 13:41:18 +0800

    UPDATE：sort下次不要用错了

--- src/demoGroup/goupStr.js
+++ src/demoGroup/goupStr.js
@@ -2,7 +2,7 @@
  * @Author: zd
  * @Date: 2024-01-04 16:00:23
  * @LastEditors: zd
- * @LastEditTime: 2024-01-04 16:49:18
+ * @LastEditTime: 2024-01-05 13:39:33
  * @Description:
  */
 // 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
@@ -10,30 +10,20 @@
 // 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 // 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-const sameArray = (arr1, arr2) => {
-  arr1.forEach(ele => {
-    if (arr2.includes(ele) === false) {
-      return false
-    }
-  })
-  return true
-}
+function groupAnagrams (strs) {
+  if (!strs.length) return []
+  let map = {}
 
-const groupAnagrams = strArray => {
-  // 1. 遍历输入的数组，按题设，会都是字符串
-  const array = []
-  for (var i = 0; i < strArray.length; i++) {
-    const subArray = [strArray[i]]
-    const sourceStrArray = strArray[i].split('')
-    for (var j = i + 1; j < strArray.length; j++) {
-      const targetStrArray = strArray[j].split('')
-      if (sameArray(sourceStrArray, targetStrArray)) {
-        subArray.push(strArray[j])
-      }
+  for (let str of strs) {
+    let sortedStr = str.split('').sort().join('')
+    if (!map[sortedStr]) {
+      map[sortedStr] = [str]
+    } else {
+      map[sortedStr].push(str)
     }
-    array.push(subArray)
   }
-  return array
+
+  return Object.values(map)
 }
 
 const res = groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
