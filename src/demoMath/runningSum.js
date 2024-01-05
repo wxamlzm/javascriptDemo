de77dfb8878c4dd05bd36d3c3069069c427f72a8
wxamlzm <zd5024@163.com>
2024-01-05 14:36:15 +0800

    UPDATE：第二种方法及描述

--- src/demoMath/runningSum.js
+++ src/demoMath/runningSum.js
@@ -18,5 +18,12 @@ var runningSum = function (nums) {
   return sumArray
 }
 
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+      nums[i] = nums[i - 1] + nums[i]
+  }
+  return nums
+};
+
 const res = runningSum([1, 2, 3, 4])
 console.log(res)
