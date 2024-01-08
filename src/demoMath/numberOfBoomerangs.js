1fa7809474cf76231a169083c3eed2e2f85878f8
wxamlzm <zd5024@163.com>
2024-01-08 11:20:39 +0800

    UPDATE：这题我连题目都看不懂

---
+++ src/demoMath/numberOfBoomerangs.js
@@ -0,0 +1,9 @@
+// 给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组 ，其中 i 和 j 之间的距离和 i 和 k 之间的欧式距离相等（需要考虑元组的顺序）。
+
+// 返回平面上所有回旋镖的数量。
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+function numberOfBoomerangs (points) {}
