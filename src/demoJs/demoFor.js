2bed70e01f78b14834f75b7370cc17504b70996d
wxamlzm <zd5024@163.com>
2024-01-10 08:15:16 +0800

    UPDATE：++i和i++

---
+++ src/demoJs/demoFor.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: zd
+ * @Date: 2024-01-09 16:38:48
+ * @LastEditors: zd
+ * @LastEditTime: 2024-01-09 16:38:51
+ * @Description: for循环基础
+ */
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i])
+}
+
+for (let i = 0; i < a.length; ++i) {
+  console.log(a[i])
+}
