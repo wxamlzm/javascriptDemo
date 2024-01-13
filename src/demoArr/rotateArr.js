469d2447972b1e32e864ecfd8587992e8ac0a327
wxamlzm <zd5024@163.com>
2024-01-13 23:08:02 +0800

    UPDATE：这中间的概念感觉更贴近数学概念

--- src/demoArr/rotateArr.js
+++ src/demoArr/rotateArr.js
@@ -1,22 +1,12 @@
 // 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
 
 var rotate = function (nums, k) {
-  const len = nums.length
-  const rotateArr = []
-  const notRotateArr = []
-  for (let i = len - 1; i >= 0; i--) {
-    if (i >= len - k) {
-      rotateArr.push(nums[i])
-    } else {
-      notRotateArr.push(nums[i])
-    }
+  const n = nums.length
+  const newArr = new Array(n)
+  for (let i = 0; i < n; ++i) {
+    newArr[(i + k) % n] = nums[i]
   }
-
-  return [...rotateArr.reverse(), ...notRotateArr.reverse()]
+  for (let i = 0; i < n; ++i) {
+    nums[i] = newArr[i]
+  }
 }
-
-const nums = [1, 2, 3, 4, 5, 6, 7],
-  k = 3
-
-const res = rotate(nums, k)
-console.log(res)
