794ce8205d0e9d14045a796f396361c9ae995d35
wxamlzm <zd5024@163.com>
2024-01-14 16:37:28 +0800

    UPDATE：链表 跳过重复值

---
+++ src/linkList/deleteDuplicates.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head || !head.next) {
+    return head
+  }
+
+  let current = head
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next // 跳过重复值
+    } else {
+      current = current.next // 移动到下一个节点
+    }
+  }
+
+  return head
+}
