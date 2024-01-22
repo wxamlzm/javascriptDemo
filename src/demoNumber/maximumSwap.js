9899eb0ae75b4db2f042bdf6787a5c04f84c024c
wxamlzm <zd5024@163.com>
2024-01-22 20:57:41 +0800

    UPDATE：先伪交换，再真实交换

--- src/demoNumber/maximumSwap.js
+++ src/demoNumber/maximumSwap.js
@@ -1,36 +1,35 @@
 // 给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值
 
+const swap = (charArray, i, j) => {
+  const temp = charArray[i]
+  charArray[i] = charArray[j]
+  charArray[j] = temp
+}
+
 /**
  * @param {number} num
  * @return {number}
  */
 var maximumSwap = function (num) {
-  let numArray = num.toString().split('')
-
-  numArray = numArray
-    .map((num, index) => {
-      return {
-        value: num,
-        index
-      }
-    })
-    .sort((a, b) => a.value - b.value)
-
-  const length = numArray.length
-
-  maxValue = numArray[length - 1].value
-  maxValueIndex = numArray[length - 1].index
-  for (let i = 0; i < length - 1; i++) {
-    if (numArray[i].value < maxValue && numArray[i].index < maxValueIndex) {
-      numArray[length - 1].index = i
-      numArray[i].index = maxValueIndex
-      break
+  const charArray = [...('' + num)]
+  const n = charArray.length
+  let maxIdx = n - 1
+  let idx1 = -1,
+    idx2 = -1
+  for (let i = n - 1; i >= 0; i--) {
+    if (charArray[i] > charArray[maxIdx]) {
+      maxIdx = i
+    } else if (charArray[i] < charArray[maxIdx]) {
+      idx1 = i
+      idx2 = maxIdx
     }
   }
-
-  numArray.sort((a, b) => a.index - b.index)
-
-  return numArray.map(ele => ele.value).join('')
+  if (idx1 >= 0) {
+    swap(charArray, idx1, idx2)
+    return parseInt(charArray.join(''))
+  } else {
+    return num
+  }
 }
 
 const num = 9973
