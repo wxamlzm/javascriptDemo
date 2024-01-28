10025659e936633472ccb1926da046d3ac9a689b
wxamlzm <zd5024@163.com>
2024-01-28 22:28:59 +0800

    UPDATE：最大公约数

--- src/demoNumber/canMeasureWater.js
+++ src/demoNumber/canMeasureWater.js
@@ -35,3 +35,21 @@ let jug1Capacity = 3,
   targetCapacity = 4
 
 canMeasureWater(jug1Capacity, jug2Capacity, targetCapacity)
+
+
+
+// var canMeasureWater = function(x, y, z) {
+//   if (x + y < z) {
+//       return false;
+//   }
+//   if (x === 0 || y === 0) {
+//       return (z === 0) || (x + y  === z);
+//   }
+//   return (z % gcd(x, y)) === 0;
+// };
+// function gcd(a, b) {
+//   if (b === 0) {
+//       return a;
+//   }
+//   return gcd(b, a % b);
+// }
