8d2e8d2a312156d04129941a494e22fbcfea67db
wxamlzm <zd5024@163.com>
2024-02-01 16:45:52 +0800

    UPDATE：不会

---
+++ src/demoArr/numsGame.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numsGame = function (nums) {
+  // 给出一个数组（排序不定）
+  // 将其设计为步长为1的等差升序数列
+  // 要求是所有方案里的最小值
+
+  // 首先枚举有办法么
+  for (let i = 0; i < nums.length; i++){
+    // 难点在于，这有两个选择，+和-
+  }
+}
+
+let nums = [3, 4, 5, 1, 6, 7]
+numsGame(nums)
