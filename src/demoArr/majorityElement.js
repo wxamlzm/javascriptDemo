e15e2c03c69ee6e6bad3fb07a3e269d945417cfb
wxamlzm <zd5024@163.com>
2024-02-05 15:36:32 +0800

    UPDATE：数学方法

--- src/demoArr/majorityElement.js
+++ src/demoArr/majorityElement.js
@@ -24,3 +24,11 @@ var majorityElement = function (nums) {
 const nums = [2, 2, 1, 1, 1, 2, 2]
 const res = majorityElement(nums)
 console.log(res)
+
+// 更新一个数学方法
+// 
+// var majorityElement = function (nums) {
+//   nums.sort();
+//   let n = nums.length;
+//   return nums[Math.floor(n / 2)];
+// };
