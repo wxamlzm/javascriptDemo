baa60a3e08903694d7d5754c400e17ce21753b33
wxamlzm <zd5024@163.com>
2024-02-06 22:24:40 +0800

    UPDATE：没有必要二层遍历

--- src/subStr/increasingTriplet.js
+++ src/subStr/increasingTriplet.js
@@ -11,27 +11,44 @@ var increasingTriplet = function (nums) {
 
   // 动态规划，每个计算每个i的节点的最长升序子序列
   // 好像不是动态规划，单纯的记事本缓存
-  const dp = new Array(nums.length).fill(0)
-  for (let i = 0; i < nums.length; i++) {
-    let maxLength = 1
-    let max = nums[i]
-    if (nums[i] === 0) {
-      console.log('a')
-    }
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] > max) {
-        maxLength++
-        dp[i] = Math.max(dp[i], maxLength)
-      } else 
-      // 这个else没想法了，因为需要递推前面的序号，感觉换个思路会更好
-      {
-        maxLength = 1
-      }
-      max = nums[j]
+  // const dp = new Array(nums.length).fill(0)
+  // for (let i = 0; i < nums.length; i++) {
+  //   let maxLength = 1
+  //   let max = nums[i]
+  //   if (nums[i] === 0) {
+  //     console.log('a')
+  //   }
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[j] > max) {
+  //       maxLength++
+  //       dp[i] = Math.max(dp[i], maxLength)
+  //     } else
+  //     // 这个else没想法了，因为需要递推前面的序号，感觉换个思路会更好
+  //     {
+  //       maxLength = 1
+  //     }
+  //     max = nums[j]
+  //   }
+  // }
+
+  // return Math.max(...dp) >= 3
+  const n = nums.length
+  if (n < 3) {
+    return false
+  }
+  let first = nums[0],
+    second = Number.MAX_VALUE
+  for (let i = 1; i < n; i++) {
+    const num = nums[i]
+    if (num > second) {
+      return true
+    } else if (num > first) {
+      second = num
+    } else {
+      first = num
     }
   }
-
-  return Math.max(...dp) >= 3
+  return false
 }
 
 let nums = [1, 5, 0, 4, 1, 3]
