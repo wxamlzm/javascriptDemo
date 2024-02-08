227d0a2e25ab77129ab05fda56a178d2a25058e2
wxamlzm <zd5024@163.com>
2024-02-08 16:06:23 +0800

    UPDATE：大概率双指针的设计就有问题

--- src/demoStr/compressString.js
+++ src/demoStr/compressString.js
@@ -17,11 +17,6 @@ var compressString = function (S) {
     // 因为放在count++后，会出现left + count 对应的并不是curWord的现象
     // 即count++后需要重新进行条件判断
     // 我认为left+count是S得下标，S的下标理所当然到S.length-1为止
-    if (left + count === S.length - 1) {
-      // 拼接末尾
-      sCompressString += word
-      sCompressString += count
-    }
 
     const curWord = S[left + count]
     if (word === curWord) {
@@ -34,6 +29,12 @@ var compressString = function (S) {
       word = curWord
       left = left + count
       count = 1
+    }
+    // 问题的核心还是在于，这个等式的时候，在当前例子下，count == 2才是正确的，但等式成立的条件却是count == 1
+    if (left + count === S.length - 1) {
+      // 拼接末尾
+      sCompressString += word
+      sCompressString += count
     }
   }
 
