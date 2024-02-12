6a694f6ea8b2323d76a14815fd4af6c6e346d353
wxamlzm <zd5024@163.com>
2024-02-12 20:24:34 +0800

    UPDATE：第二遍了还是不行，双指针

--- src/demoMath/maxArea.js
+++ src/demoMath/maxArea.js
@@ -22,3 +22,18 @@ var maxArea = function (height) {
 
   return maxArea
 }
+
+
+// var maxArea = function(height) {
+//   let ans = 0, left = 0, right = height.length - 1;
+//   while (left < right) {
+//       const area = (right - left) * Math.min(height[left], height[right]);
+//       ans = Math.max(ans, area);
+//       if (height[left] < height[right]) {
+//           left++;
+//       } else {
+//           right--;
+//       }
+//   }
+//   return ans;
+// };
