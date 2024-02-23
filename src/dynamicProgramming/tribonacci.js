0b3f98505b12dffa968073af019984546730063e
wxamlzm <zd5024@163.com>
2024-02-23 10:45:45 +0800

    UPDATE：用动态规划优化

--- src/dynamicProgramming/tribonacci.js
+++ src/dynamicProgramming/tribonacci.js
@@ -8,14 +8,28 @@
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function (n) {
-  if (n === 0) return 0
-  if (n === 1) return 1
-  if (n === 2) return 1
+// var tribonacci = function (n) {
+//   if (n === 0) return 0
+//   if (n === 1) return 1
+//   if (n === 2) return 1
 
-  return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+//   return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+// }
+
+var tribonacci = function (n) {
+  const dp = new Array(n).fill(0)
+
+  dp[0] = 0
+  dp[1] = 1
+  dp[2] = 1
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+  }
+
+  return dp[n]
 }
 
 let n = 4
 const res = tribonacci(n)
-console.log(res)
+// console.log(res)
