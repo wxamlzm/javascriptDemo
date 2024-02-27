d36c2b7f8724af97e229fc2bacc9ff95e2d53745
wxamlzm <zd5024@163.com>
2024-02-27 10:17:39 +0800

    UPDATE：新建单项链表，链表用第一个节点表示

--- src/srcAlgo/linkList/mergeTwoLists.js
+++ src/srcAlgo/linkList/mergeTwoLists.js
@@ -6,4 +6,17 @@ const SingleLinkNode = linkList.SingleLinkNode
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {}
+var mergeTwoLists = function (list1, list2) {
+  console.log(list1.next)
+}
+
+const n0 = new SingleLinkNode(1)
+const n1 = new SingleLinkNode(2)
+const n2 = new SingleLinkNode(4)
+
+n0.next = n1
+n1.next = n2
+
+let list1 = [1, 2, 4],
+  list2 = [1, 3, 4]
+mergeTwoLists(n0, list2)
