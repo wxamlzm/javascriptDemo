7b1c636be0486bd18ded5a9f9520827cf07904c4
wxamlzm <zd5024@163.com>
2024-03-20 10:46:14 +0800

    UPDATE：待完成题目归拢

---
+++ src/srcAlgo/temp/detectCycle.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: zd
+ * @Date: 2024-03-20 10:35:46
+ * @LastEditors: zd
+ * @LastEditTime: 2024-03-20 10:36:16
+ * @Description: 
+ */
+const linkList = require('../../utils/linkList')
+const SingleLinkNode = linkList.SingleLinkNode
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    
+};
