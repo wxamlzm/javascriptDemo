8e8587f539d11406e8d3b6ed61db692770453518
wxamlzm <zd5024@163.com>
2024-03-29 09:36:02 +0800

    UPDATE

--- src/srcAlgo/TreeNode/levelOrder.js
+++ src/srcAlgo/TreeNode/levelOrder.js
@@ -1,3 +1,5 @@
+// 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
---
+++ src/srcAlgo/TreeNode/zigzagLevelOrder.js
@@ -0,0 +1,15 @@
+// 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {}
