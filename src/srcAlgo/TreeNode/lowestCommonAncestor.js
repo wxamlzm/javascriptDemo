b2fb65500ed24cc97de2cf2b6ace655967510840
wxamlzm <zd5024@163.com>
2024-04-02 08:32:57 +0800

    UPDATE

--- src/srcAlgo/TreeNode/lowestCommonAncestor.js
+++ src/srcAlgo/TreeNode/lowestCommonAncestor.js
@@ -5,7 +5,6 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-  // 首先我没有完全理解什么是最近公共祖先，主要体现在，看图能看懂，但没法用自己的语言描述出来
   if (root === null || root === p || root === q) return root
   const left = lowestCommonAncestor(root.left, p, q)
   const right = lowestCommonAncestor(root.right, p, q)
