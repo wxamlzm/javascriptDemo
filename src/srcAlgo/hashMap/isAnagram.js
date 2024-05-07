5dd69eb4af86a4768f4de70122e2d7211d8d7904
wxamlzm <zd5024@163.com>
2024-05-07 16:28:02 +0800

    UPDATE：更新版本

--- src/srcAlgo/hashMap/isAnagram.js
+++ src/srcAlgo/hashMap/isAnagram.js
@@ -38,3 +38,53 @@ let s = 'anagram',
   t = 'nagaram'
 const res = isAnagram(s, t)
 console.log(res)
+
+
+// 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+// 两个字符串是字母异位词,需要满足以下特征:
+
+// 包含相同种类的字母:两个字符串包含的字母种类是完全相同的。
+
+// 字母出现的频次相同:两个字符串中每个字母出现的次数是相同的。
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) return false
+
+//   function getStrMap (str) {
+//     const strMap = {}
+//     for (let i = 0; i < str.length; i++) {
+//       if (strMap[str[i]]) {
+//         strMap[str[i]]++
+//       } else {
+//         strMap[str[i]] = 1
+//       }
+//     }
+//     return strMap
+//   }
+
+//   const sMap = getStrMap(s)
+//   const tMap = getStrMap(t)
+
+//   for (const key in sMap) {
+//     const sMapCount = sMap[key]
+//     const tMapCount = tMap[key]
+
+//     if (sMapCount !== tMapCount) {
+//       return false
+//     }
+//   }
+
+//   return true
+// }
+
+// const s = 'rat'
+// const t = 'car'
+// const res = isAnagram(s, t)
+// console.log(res)
+
