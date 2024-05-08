f21d8284ccf0ef9d9dc585770cfbf49829fcffd7
wxamlzm <zd5024@163.com>
2024-05-08 21:01:04 +0800

    UPDATE：错了

--- src/srcAlgo/递归/permuteUnique.js
+++ src/srcAlgo/递归/permuteUnique.js
@@ -4,9 +4,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function(nums) {
+var permuteUnique = function (nums) {
+  const res = []
+  const len = nums.length
 
-};
+  const dfs = start => {      
+    if (start === len) {
+      res.push([...nums])
+      return
+    }
 
+    const disc = []
 
-const nums = [1,1,2]
+    for (let i = start; i < len; i++) {
+      if(nums[i] in disc) continue
+      disc.push(nums[i])
+      [nums[start], nums[i]] = [nums[i], nums[start]]
+      dfs(start + 1);
+      [nums[start], nums[i]] = [nums[i], nums[start]]
+    }
+  }
+
+  dfs(0)
+  return res
+}
+
+const nums = [1, 1, 2]
+permuteUnique(nums)
