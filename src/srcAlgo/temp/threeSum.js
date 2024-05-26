f51f2d259d2e775f6ab28c1859339be67f546f14
wxamlzm <zd5024@163.com>
2024-05-26 20:07:20 +0800

    UPDATE

---
+++ src/srcAlgo/temp/threeSum.js
@@ -0,0 +1,28 @@
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+// 你返回所有和为 0 且不重复的三元组。
+
+// 注意：答案中不可以包含重复的三元组。
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const res = []
+  // nums 去重
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          res.push([nums[i], nums[j], nums[k]])
+        }
+      }
+    }
+  }
+  return res
+}
+
+const nums = [-1, 0, 1, 2, -1, -4]
+const res = threeSum(nums)
+console.log(res)
