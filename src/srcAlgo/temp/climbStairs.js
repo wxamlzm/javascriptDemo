bd5ca7e6b5ef08b43f3cd6ea4b0943b3d5280b07
wxamlzm <zd5024@163.com>
2024-06-10 21:53:34 +0800

    UPDATE：做了一小时。。。

--- src/srcAlgo/temp/climbStairs.js
+++ src/srcAlgo/temp/climbStairs.js
@@ -6,13 +6,29 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function (n) {
-  if (n === 0) return 1
-  if (n === 1) return 1
+// var climbStairs = function (n) {
+//   if (n === 0) return 1
+//   if (n === 1) return 1
 
-  // 走到n-1后1步，走到n-2后两步
-  return climbStairs(n - 1) + climbStairs(n - 2)
+//   // 走到n-1后1步，走到n-2后两步
+//   return climbStairs(n - 1) + climbStairs(n - 2)
+// }
+
+var climbStairs = function (n) {
+  const climbStairsCountArr = new Array(n + 1).fill(0)
+
+  for (let i = 0; i <= n; i++) {
+    if (i === 0 || i === 1) {
+      climbStairsCountArr[i] = 1
+    } else {
+      climbStairsCountArr[i] =
+        climbStairsCountArr[i - 1] + climbStairsCountArr[i - 2]
+    }
+  }
+
+  return climbStairsCountArr[n]
 }
 
-const res = climbStairs(5)
+const res = climbStairs(44)
+
 console.log(res)
