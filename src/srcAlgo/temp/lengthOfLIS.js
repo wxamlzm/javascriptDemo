bcb55568635251a88cd2a1f4d3ba33d42cad39ec
wxamlzm <zd5024@163.com>
2024-07-20 19:04:56 +0800

    UPDATE：节省空间，用len替换数组本身，但时间复杂度没变

--- src/srcAlgo/temp/lengthOfLIS.js
+++ src/srcAlgo/temp/lengthOfLIS.js
@@ -28,6 +28,21 @@ var lengthOfLIS = function (nums) {
   return maxLen
 }
 
+var lengthOfLIS = function (nums) {
+  if (nums.length === 0) return 0
+  const dp = new Array(nums.length).fill(1)
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+    maxLen = Math.max(maxLen, dp[i])
+  }
+
+  return maxLen
+}
 const nums = [0, 1, 0, 3, 2, 3]
 
 const res = lengthOfLIS(nums)
