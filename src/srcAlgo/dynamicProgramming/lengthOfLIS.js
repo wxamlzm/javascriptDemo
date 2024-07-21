2c9c08c74cda3019da792bf7c13f49164ae3eac9
wxamlzm <zd5024@163.com>
2024-07-21 18:36:33 +0800

    UPDATE：二分是真想不到

--- src/srcAlgo/dynamicProgramming/lengthOfLIS.js
+++ src/srcAlgo/dynamicProgramming/lengthOfLIS.js
@@ -6,44 +6,75 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums) {
-  // 建立数组缓存所有子序列
-  const sequences = nums.map(() => [])
-  // 初始化
-  let maxLen = 1
+// var lengthOfLIS = function (nums) {
+//   // 建立数组缓存所有子序列
+//   const sequences = nums.map(() => [])
+//   // 初始化
+//   let maxLen = 1
 
-  // 遍历 穷举节点
-  for (let i = 0; i < nums.length; i++) {
-    // 初始化数组
-    sequences[i].push(nums[i])
-    // 遍历所有i之前的子数组
-    for (let j = 0; j < i; j++) {
-      // 判断递增
-      if (nums[i] > nums[j] && sequences[j].length + 1 > sequences[i].length) {
-        sequences[i] = [...sequences[j], nums[i]]
-      }
-    }
-    maxLen = Math.max(maxLen, sequences[i].length)
-  }
-  return maxLen
-}
+//   // 遍历 穷举节点
+//   for (let i = 0; i < nums.length; i++) {
+//     // 初始化数组
+//     sequences[i].push(nums[i])
+//     // 遍历所有i之前的子数组
+//     for (let j = 0; j < i; j++) {
+//       // 判断递增
+//       if (nums[i] > nums[j] && sequences[j].length + 1 > sequences[i].length) {
+//         sequences[i] = [...sequences[j], nums[i]]
+//       }
+//     }
+//     maxLen = Math.max(maxLen, sequences[i].length)
+//   }
+//   return maxLen
+// }
 
 // 空间优化
-var lengthOfLIS = function (nums) {
-  if (nums.length === 0) return 0
-  const dp = new Array(nums.length).fill(1)
+// var lengthOfLIS = function (nums) {
+//   if (nums.length === 0) return 0
+//   const dp = new Array(nums.length).fill(1)
 
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1)
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (nums[i] > nums[j]) {
+//         dp[i] = Math.max(dp[i], dp[j] + 1)
+//       }
+//     }
+//     maxLen = Math.max(maxLen, dp[i])
+//   }
+
+//   return maxLen
+// }
+
+// 时间优化
+
+var lengthOfLIS = function (nums) {
+  const tails = []
+
+  for (let num of nums) {
+    let left = 0
+    right = tails.length
+
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2)
+      if (tails[mid] < num) {
+        left = mid + 1
+      } else {
+        right = mid
       }
     }
-    maxLen = Math.max(maxLen, dp[i])
+
+    if (left == tails.length) {
+      tails.push(num)
+    } else {
+      tails[left] = num
+    }
   }
 
-  return maxLen
+  console.log(tails)
+
+  return tails.length
 }
+
 const nums = [0, 1, 0, 3, 2, 3]
 
 const res = lengthOfLIS(nums)
