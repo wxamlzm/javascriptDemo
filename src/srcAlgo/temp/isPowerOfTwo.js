efae556ae62715e806283377ba8ab06ebceeb6bb
wxamlzm <zd5024@163.com>
2024-08-23 18:08:51 +0800

    UPDATE：蠢办法

---
+++ src/srcAlgo/temp/isPowerOfTwo.js
@@ -0,0 +1,21 @@
+// 给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
+
+// 如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  let exam = 1
+  if (n === exam) return true
+  for (let i = 0; i < 32; i++) {
+    exam *= 2
+    if (n === exam) return true
+  }
+  return false
+}
+
+const n = 3
+const res = isPowerOfTwo(n)
+console.log(res)
