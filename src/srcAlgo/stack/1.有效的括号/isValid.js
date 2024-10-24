ec5d2d3c73896da0fd5964736b1b35adf5189518
wxamlzm <zd5024@163.com>
2024-10-24 16:13:11 +0800

    FUXI-1

--- src/srcAlgo/stack/1.有效的括号/isValid.js
+++ src/srcAlgo/stack/1.有效的括号/isValid.js
@@ -1,41 +1,51 @@
-// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
-
-// 有效字符串需满足：
-
-// 左括号必须用相同类型的右括号闭合。
-// 左括号必须以正确的顺序闭合。
-// 每个右括号都有一个对应的相同类型的左括号。
+/**
+ * 判断字符串中的括号是否合法配对
+ * @param {string} s 只包含 '('，')'，'{'，'}'，'['，']' 的字符串
+ * @return {boolean} 括号是否合法配对
+ *
+ * 要求：
+ * 1. 左括号必须用相同类型的右括号闭合
+ * 2. 左括号必须以正确的顺序闭合
+ * 3. 每个右括号都有一个对应的相同类型的左括号
+ */
 
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function (s) {
-  const sObj = {
-    '(': ')',
-    '[': ']',
-    '{': '}'
-  }
+  // 建立括号对应关系映射
+  const bracketPairs = new Map()
+  bracketPairs.set('[', ']')
+  bracketPairs.set('(', ')')
+  bracketPairs.set('{', '}')
 
-  const stack = []
+  // 使用栈存储待匹配的左括号
+  const leftBrackets = []
 
-  for (let char of s) {
-    // let of
-    if (char in sObj) {
-      stack.push(char)
+  for (let bracket of s) {
+    if (bracketPairs.has(bracket)) {
+      // 如果是左括号，入栈等待匹配
+      leftBrackets.push(bracket)
     } else {
-      if (stack.length === 0) return false
-      const stackTop = stack.pop()
-      const expectedRightBracket = sObj[stackTop]
-      if (expectedRightBracket !== char) {
+      // 这是仅有右括号的情况
+      // 如果栈为空，说明右括号无法找到匹配的左括号
+      if (leftBrackets.length === 0) return false
+
+      // 去除最近的左括号进行匹配
+      const lastLeftBracket = leftBrackets.pop()
+      const expectedRightBracket = bracketPairs.get(lastLeftBracket)
+      if (expectedRightBracket !== bracket) {
         return false
       }
     }
   }
-  return stack.length === 0
+
+  // 所有括号都应该完成匹配，栈应该为空
+  return leftBrackets.length === 0
 }
 
-let s = ']'
+let s = '[({})]'
 
 const res = isValid(s)
 console.log(res)
