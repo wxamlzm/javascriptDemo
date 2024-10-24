71335a937555e11389ab575fcf9d384f17fec2f5
wxamlzm <zd5024@163.com>
2024-10-24 21:36:57 +0800

    UPDATE：更新最小栈和学习记录

--- src/srcAlgo/stack/3. 最小栈/minStack.js
+++ src/srcAlgo/stack/3. 最小栈/minStack.js
@@ -1,27 +1,33 @@
-// 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+/*
+  设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
 
-// 实现 MinStack 类:
-//     MinStack() 初始化堆栈对象。
-//     void push(int val) 将元素val推入堆栈。
-//     void pop() 删除堆栈顶部的元素。
-//     int top() 获取堆栈顶部的元素。
-//     int getMin() 获取堆栈中的最小元素。
+  实现 MinStack 类:
+    MinStack() 初始化堆栈对象。
+    void push(int val) 将元素val推入堆栈。
+    void pop() 删除堆栈顶部的元素。
+    int top() 获取堆栈顶部的元素。
+    int getMin() 获取堆栈中的最小元素。
+*/
 
 var MinStack = function () {
-  this.stack = []
-  this.minValues = []
+  // 主栈：存储所有元素
+  this.dataStack = []
+  // 辅助栈：存储每个阶段的最小值
+  this.minStack = []
 }
 
 /**
- * @param {number} val
- * @return {void}
+ * 将新元素推入栈中
+ * @param {number} val - 要推入的值
  */
 MinStack.prototype.push = function (val) {
-  this.stack.push(val)
-
-  // 如果 minValues 为空或者新值小于等于当前最小值，将新值添加到 minValues 中
-  if (this.minValues.length === 0 || val <= this.getMin()) {
-    this.minValues.push(val)
+  this.dataStack.push(val)
+  // 如果辅助栈为空新元素小于等于当前最小值，则将新元素推入辅助栈
+  if (
+    this.minStack.length === 0 ||
+    val <= this.minStack[this.minStack.length - 1]
+  ) {
+    this.minStack.push(val)
   }
 }
 
@@ -29,37 +35,28 @@ MinStack.prototype.push = function (val) {
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-  if (this.stack.length === 0) throw new Error('栈为空')
+  if (this.dataStack.length === 0) return
 
-  // 只有当弹出的元素等于当前最小值时，才从 minValues 中移除
-  if (this.stack[this.stack.length - 1] === this.getMin()) {
-    this.minValues.pop()
+  // 如果弹出的元素是当前最小值，辅助栈也要弹出
+  if (
+    this.dataStack[this.dataStack.length - 1] ===
+    this.minStack[this.minStack.length - 1]
+  ) {
+    this.minStack.pop()
   }
-
-  this.stack.pop()
+  this.dataStack.pop()
 }
 
 /**
- * @return {number}
+ * @return {number} // top不能改变数组
  */
 MinStack.prototype.top = function () {
-  if (this.stack.length === 0) throw new Error('栈为空')
-  return this.stack[this.stack.length - 1]
+  return this.dataStack[this.dataStack.length - 1]
 }
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-  if (this.stack.length === 0) throw new Error('栈为空')
-  return this.minValues[this.minValues.length - 1]
+  return this.minStack[this.minStack.length - 1]
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
