7f38f2cdc9d6f7747add03b7dab08a7e2de6b042
wxamlzm <zd5024@163.com>
2024-10-29 23:02:34 +0800

    UPDATE：整理

--- src/srcAlgo/doublePointer/移除数组中指定元素/removeElement.js
+++ src/srcAlgo/doublePointer/移除数组中指定元素/removeElement.js
@@ -10,17 +10,24 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  let i = 0
-  while (i < nums.length) {
-    if (nums[i] === val) {
-      nums.splice(i, 1)
-    } else {
-      i++
-    }
-  }
-  console.log(nums)
-  return nums.length
+
 }
+
+
+
+// js中splice的应用
+// var removeElement2 = function (nums, val) {
+//   let i = 0
+//   while (i < nums.length) {
+//     if (nums[i] === val) {
+//       nums.splice(i, 1)
+//     } else {
+//       i++
+//     }
+//   }
+//   console.log(nums)
+//   return nums.length
+// }
 
 let nums = [0, 1, 2, 2, 3, 0, 4, 2],
   val = 2
